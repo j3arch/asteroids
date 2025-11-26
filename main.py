34fa8442d1e92eadb1094a9b99eb828c89896d5f
j3arch <j3archx@gmail.com>
2025-11-26 23:27:53 +0300

    added game state

--- main.py
+++ main.py
@@ -1,61 +1,62 @@
+import sys
 import pygame
 from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
 from shot import *
+from logger import log_event, log_state
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-
-
-    # Groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
-
-    Player.containers = (updatable, drawable)
 
     Asteroid.containers = (asteroids, updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
-    # Game loop
     while True:
+        log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
         for obj in drawable:
             obj.draw(screen)
 
-        for asteroid in asteroids:               # Check for player-asteroid collisions
-            if player.collides_with(asteroid):
-                print("Game over!")
-                import sys
-                sys.exit()
+        pygame.display.flip()
 
-            for shot in shots:                   # Bullet-asteroid collision
-                if shot.collides_with(asteroid):
-                    asteroid.split()
-                    shot.kill()
-
-
-        pygame.display.flip()  # Update the display
-
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-        
 
 
 if __name__ == "__main__":
